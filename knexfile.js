08f93d3e6dd95f6b7cc85d1ecf80206a054fc01e
Brandon Noad <Brandon.Noad@gmail.com>
2019-02-24 14:58:36 -0500

    Update knexfile and package.json for Heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { MEAL_PLANNER_DB_HOST, MEAL_PLANNER_DB_USER, MEAL_PLANNER_DB_DATABASE } = process.env;
-
 module.exports = {
     development: {
         client: 'sqlite3',
@@ -13,9 +11,8 @@ module.exports = {
     production: {
         client: 'pg',
         connection: {
-            host: MEAL_PLANNER_DB_HOST,
-            user: MEAL_PLANNER_DB_USER,
-            database: MEAL_PLANNER_DB_DATABASE
+            connectionString: process.env.DATABASE_URL,
+            ssl: true
         }
-     }
+    }
 };
